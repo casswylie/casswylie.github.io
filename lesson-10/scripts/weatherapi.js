314e39635dfc4269147753bb8c0e3de5d670bd72
Cassie Wylie <cassiemichele@gmail.com>
2019-03-19 10:16:48 -0600

    API current weather on preston page

---
+++ lesson-10/scripts/weatherapi.js
@@ -0,0 +1,34 @@
+
+var weatherRequest = new XMLHttpRequest();
+
+//var apiURLstring = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial';
+
+weatherRequest.open('GET', '//api.openweathermap.org/data/2.5/weather?id=5604473&appid=51c5895f12b5d16e4a8afa4b12d68ab0&units=imperial', true);
+
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById('currentTemp').innerHTML = weatherData.main.temp;
+
+    document.getElementById('description').innerHTML = weatherData.weather["0"].main;
+
+    document.getElementById('lowTemp').innerHTML = weatherData.main.temp_min;
+
+    document.getElementById('highTemp').innerHTML = weatherData.main.temp_max;
+
+    document.getElementById('windSpeed').innerHTML = weatherData.wind.speed;
+
+    document.getElementById('humidity').innerHTML = weatherData.main.humidity;
+
+    windChill();
+
+    var iconcode = weatherData.weather[0].icon;
+    var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    document.getElementById('weather_icon').src = icon_path;
+
+    
+} //end of function
+
--- lesson-10/scripts/windchill.js
+++ lesson-10/scripts/windchill.js
@@ -9,3 +9,22 @@ function windChill() {
     f = f.toFixed(2);
     document.getElementById("windChill").innerHTML = f;
 }
+
+/*var windRequest = new XMLHttpRequest();
+windRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=51c5895f12b5d16e4a8afa4b12d68ab0&units=imperial', true);
+windRequest.send();
+
+
+windRequest.onload = function windChill() {
+
+
+    var low = document.getElementById('lowTemp').innerHTML = weatherData.main.temp_min; //average of high and low temperature in Farenheit
+    var high = document.getElementById('highTemp').innerHTML = weatherData.main.temp_max;
+    var average = (low + high) /2;
+    var s = document.getElementById('windSpeed').innerHTML = weatherData.wind.speed; //windspeed in mph
+    var expo = Math.pow(s, 0.16); //windspeed with exponant
+    
+    var f  = 35.74 + (0.6215 * average) -(35.75 * expo) + (0.4275 * average * expo); //windchill factor in Fahrenheit
+    f = f.toFixed(2);
+    document.getElementById("windChill").innerHTML = f;
+}*/
